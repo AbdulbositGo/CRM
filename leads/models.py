59c9f00f57273b5b5aee0149e5707d206335c206
Abdulbosit <101085020+AbdulbositGo@users.noreply.github.com>
2022-10-21 13:44:17 -0700

    created Models

---
+++ leads/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+
+User = get_user_model()
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
