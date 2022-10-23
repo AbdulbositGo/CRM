36035c0c9fb520f99bf2e3df384fe55b10c41b80
Abdulbosit <101085020+AbdulbositGo@users.noreply.github.com>
2022-10-23 08:14:17 -0700

    tailwind css and CRUD are added

--- leads/views.py
+++ leads/views.py
@@ -3,6 +3,10 @@ from .models import Lead
 from .forms import LeadModelForm
 
 
+def home(request):
+    return render(request, 'home.html')
+
+
 def lead_list(request):
     leads = Lead.objects.all()
 
@@ -43,7 +47,8 @@ def lead_update(request, pk):
             return redirect("/lead/list")
     context = {
         'form': form,
-    }
+        'lead': lead
+        }
     return render(request, "lead_update.html", context)
 
 
