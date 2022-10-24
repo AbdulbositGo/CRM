291b5322a435e5ab19595c03da3dac738609d4f7
Abdulbosit <101085020+AbdulbositGo@users.noreply.github.com>
2022-10-24 11:56:26 -0700

    static files are connected

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from leads.views import home
 
 
@@ -7,5 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('lead/', include("leads.urls")),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
